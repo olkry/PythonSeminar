d1e607992ef3ed33db54a25d6d7e75dfa772a538
olkry <adenos90@gmail.com>
2023-05-25 21:58:12 +0300

    added homework after the 7th workshop

--- phone_book_DZ/methods.py
+++ phone_book_DZ/methods.py
@@ -1,25 +1,47 @@
 # Методы для работы программы.
 
-def read_data(phones_list):
+def read_data(phones_list):  # Считывает данные из файла
     with open(phones_list, 'r', encoding='utf-8') as file:
         data_list = file.readlines()
         data_list = [x.rstrip().split(':') for x in data_list]
     return data_list
 
 
-def look_data(phones):
+def look_data(phones):  # Выводит данные из принимаемого списка
     for i, item in enumerate(phones, 1):
         print(i, ', '.join(item))
 
 
-def add_data(phones_list):
+def add_data(phones_list):  # Добовляет данные в список
     with open(phones_list, 'a', encoding='utf-8') as file:
         name, phone, comment = input("Введите ФИО:\t"), input("Введите телефон:\t"), \
             input("Введите комментарий:\t")
-        string = [name+':', phone+':', comment]
-        file.writelines(string)
-
-# add_data("test.txt")
-# look_data(read_data("test.txt"))
+        string_add = [name + ':', phone + ':', comment + '\n']
+        file.writelines(string_add)
 
 
+def del_data(phone_list, del_line):
+    with open(phone_list, 'r', encoding='utf-8') as file:
+        losting_data = file.readlines()
+        losting_data.pop(del_line - 1)
+    with open(phone_list, 'w', encoding='utf-8') as file:
+        file.writelines(losting_data)
+
+
+def upd_data(phone_list, upd_line, new_data):
+    with open(phone_list, 'r', encoding='utf-8') as file:
+        upd_list = file.readlines()
+        upd_list = [x.rstrip().split(':') for x in upd_list]
+    upd_list[upd_line - 1] = new_data
+    with open(phone_list, 'w', encoding='utf-8') as file:
+        for i in upd_list:
+            file.writelines(':'.join(i) + '\n')
+
+
+# look_data(read_data('test.txt'))
+# name, phones, comment = input("Введите ФИО:\t"), input("Введите телефон:\t"), \
+#     input("Введите комментарий:\t")
+# string_upd = [name, phones, comment]
+# upded_line = int(input('Напишите номер контакта, для удаления, или нажмите 0, чтобы прервать операцию.\t'))
+# upd_data('test.txt', upded_line, string_upd)
+# look_data(read_data('test.txt'))
--- phone_book_DZ/phone_book_code.py
+++ phone_book_DZ/phone_book_code.py
@@ -1,12 +1,75 @@
 from methods import *
 
 print("Добро пожаловать!", "Телефонная книга версии pre.alpha.0.055. к вашим услугам.",
-      "В настоящее время доступны следующие команды:", "1. Просмотреть контакты", sep='\n')
+      "В настоящее время доступны следующие команды:", "1. Просмотреть контакты", '2. Добавить новый контакт',
+      '3. Изменить контакт', '4. Удалить контакт', "0. Завершить работу программы", sep='\n')
 choice = int(input('Введите команду:\t'))
 
 if choice == 1:
+    print()
     look_data(read_data("input_data.txt"))
+    print()
+    input("Нажмите любую клавышу для завершения")
+
 elif choice == 2:
     add_data("input_data.txt")
+    print()
+    print('Данные успешно сохранены')
+    print()
+    choice_count = int(
+        input('Если желате просмотреть список контактов, нажмите 1, нажмите 0 - закрыть программу.\t'))
+    if choice_count == 1:
+        look_data(read_data("input_data.txt"))
+        print()
+        input("Нажмите любую клавышу для завершения")
+
+    else:
+        exit()
+
+elif choice == 3:
+    look_data(read_data("input_data.txt"))
+    print()
+    upded_line = int(input('Напишите номер контакта, для редактирования, или нажмите 0, чтобы прервать операцию.\t'))
+    if upded_line == 0:
+        print('Программа завершена, нажмите любую клавишу')
+        exit()
+    else:
+        name, phones, comment = input("Введите новое ФИО для контакта:\t"), input("Введите новый телефон:\t"), \
+            input("Введите новый комментарий:\t")
+        string_upd = [name, phones, comment]
+        upd_data("input_data.txt", upded_line, string_upd)
+        print("Успешно обновленно.")
+        choice_count = int(
+            input('Если желате просмотреть список контактов, нажмите 1, или 0 - закрыть программу.\t'))
+        if choice_count == 1:
+            look_data(read_data("input_data.txt"))
+            print()
+            input("Нажмите любую клавишу для завершения")
+        else:
+            exit()
+
+elif choice == 4:
+    look_data(read_data("input_data.txt"))
+    print()
+    delited_line = int(input('Напишите номер контакта, для удаления, или нажмите 0, чтобы прервать операцию.\t'))
+    if delited_line == 0:
+        print('Программа завершена, нажмите любую клавишу')
+        exit()
+    else:
+        del_data("input_data.txt", delited_line)
+        print('Данные успешно сохранены')
+        choice_count = int(
+            input('Если желате просмотреть список контактов, нажмите 1, или 0 - закрыть программу.\t'))
+        if choice_count == 1:
+            look_data(read_data("input_data.txt"))
+            print()
+            input("Нажмите любую клавишу для завершения")
+        else:
+            exit()
+
+
+elif choice == 0:
+    print('Всего доброго!')
+
 else:
     print("Неверная команда!!!")
