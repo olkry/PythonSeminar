55c5745cbb0cbca5cabb4db19c3f0e00798359a0
olkry <adenos90@gmail.com>
2023-05-27 22:43:46 +0300

    add seminar009

---
+++ Seminar009 moduls/controller.py
@@ -0,0 +1,46 @@
+import model
+import view
+import text
+
+
+def start():
+    while True:
+        choice = view.main_menu()
+
+        match choice:
+            case 1:
+                model.open_pb()
+                view.print_message(text.load_successful)
+            case 2:
+                model.save_pb()
+                view.print_message(text.save_successful)
+            case 3:
+                pb = model.get_pb()
+                view.prunt_contact(pb, text.pb_empety)
+            case 4:
+                contact = view.input_contact(text.input_new_contact)
+                name = model.add_contact(contact)
+                view.print_message(text.new_contact_successful(name))
+            case 5:
+                key_word = view.input_search(text.input_search)
+                result = model.search_contact(key_word)
+                view.prunt_contact(result, text.empty_search(key_word))
+            case 6:
+                key_word = view.input_search(text.input_change)
+                result = model.search_contact(key_word)
+                if result:
+                    if len(result) != 1:
+                        view.prunt_contact(result, '')
+                        current_id = view.input_search(text.input_index)
+                    else:
+                        current_id = result[0].get('id')
+                    new_contact = view.input_contact(text.change_contact)
+                    name = model.change_contact(new_contact, current_id)
+                    view.print_message(text.change_successful(name))
+                else:
+                    view.print_message(text.empty_search(key_word))
+
+            case 7:
+                pass
+            case 8:
+                break
